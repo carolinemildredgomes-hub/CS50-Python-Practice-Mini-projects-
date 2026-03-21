3610645aa8aa4b6d33359deefe4032f5de48b118
Caroline Mildred Gomes <carolinemildredgomes@gmail.com>
2026-03-21 22:00:08 +0600

    Delete integer.py

--- integer.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        number = int(input("Number: "))
-        print("Valid integer:" , number)
-        break
-    except ValueError:
-        print("Inavild input , please enter an integer.")
