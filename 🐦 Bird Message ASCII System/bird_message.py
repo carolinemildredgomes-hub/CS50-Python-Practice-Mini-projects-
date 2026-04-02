a3025324810d3bb6af58b7bfc825c8e2f830e861
Caroline Mildred Gomes <carolinemildredgomes@gmail.com>
2026-04-02 21:36:38 +0600

    bird_message.py

---
+++ 🐦 Bird Message ASCII System/bird_message.py
@@ -0,0 +1,11 @@
+def bird_message(message):
+    print("\n   \\")
+    print("   (o>")
+    print("  //\\\\")
+    print("  V_/_")
+    print()
+    print("Bird says:", message)
+
+
+text = input("Enter your message: ")
+bird_message(text)
